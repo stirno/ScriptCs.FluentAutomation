329178e172a8f9759bc3832f320b4793628532b9
Brandon Stirnaman <stirno@gmail.com>
2013-09-28 12:21:21 -0500

    Missing file

---
+++ TestResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCs.FluentAutomation
+{
+    public class TestResult
+    {
+        public string Name { get; set; }
+
+        public bool Passed { get; set; }
+
+        public string Message { get; set; }
+
+        public Exception Exception { get; set; }
+    }
+}
